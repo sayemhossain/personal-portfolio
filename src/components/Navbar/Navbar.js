054faea326526a5d1d2b97747ce65d2c68ba11a6
SAYAM HOSSAIN <shayem15-12011@diu.edu.bd>
2022-06-04 01:02:19 +0600

    working on navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,11 +1,28 @@
 import React from "react";
 import "./Navbar.css";
 const Navbar = () => {
+  let links = [
+    { name: "HOME", link: "/" },
+    { name: "SERVICE", link: "/" },
+    { name: "ABOUT", link: "/" },
+    { name: "SKILLS", link: "/" },
+    { name: "BLOGS", link: "/" },
+    { name: "CONTACT", link: "/" },
+  ];
   return (
     <div className="navbar shadow-lg w-full fixed top-0 left-0 md:px-10 px-7">
       <div className="md:flex py-4">
         <div className="font-bold text-2xl cursor-pointer ">Portfolio</div>
       </div>
+      <ul>
+        {links.map((link) => (
+          <li>
+            <a className="text-white" href={link.link}>
+              {link.name}
+            </a>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
