8d99407804225f5b7f41df2cca50ae859fdbbd86
SAYAM HOSSAIN <shayem15-12011@diu.edu.bd>
2022-06-03 16:07:34 +0600

    tailwind install done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Navbar></Navbar>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="bg-slate-500">
+      <h1>hlw</h1>
+    </div>
+  );
+};
+
+export default Navbar;
